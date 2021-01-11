a9e9bfa1b40180ce9536dea3c377491631e5b5b2
Jack Vine <jackv949@gmail.com>
2021-01-11 21:33:10 +1030

    Connected to Firebase Android app (google-services.json ignored because it contains this info)

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,49 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ms_material_color/ms_material_color.dart';
 
 import 'home_page/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'ComicWrap',
-        theme: ThemeData(
-          primarySwatch: MsMaterialColor(0xffe91e63),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: HomePage());
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        Widget homeWidget;
+        if (snapshot.connectionState == ConnectionState.done) {
+          homeWidget = HomePage();
+        } else {
+          Widget body;
+          if (snapshot.hasError) {
+            body = Text("Failed to connect to Firebase.");
+          } else {
+            body = Text("Connecting to Firebase...");
+          }
+
+          homeWidget = Scaffold(
+            appBar: AppBar(
+              title: Text("ComicWrap"),
+            ),
+            body: body,
+          );
+        }
+
+        return MaterialApp(
+            title: 'ComicWrap',
+            theme: ThemeData(
+              primarySwatch: MsMaterialColor(0xffe91e63),
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            home: homeWidget);
+      },
+    );
   }
 }
