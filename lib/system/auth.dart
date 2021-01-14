4df897b34e4449032c55d1e21d64eecbefe530bb
Jack Vine <jackv949@gmail.com>
2021-01-14 21:25:34 +1030

    Basic email auth working. TODO: Error states, polish

--- lib/system/auth.dart
+++ lib/system/auth.dart
@@ -27,49 +27,7 @@ Future<void> linkGoogleAuth(BuildContext context) async {
     print('Account link failed with error code: ${e.code}');
     // Account is already linked to another user
     if (e.code == 'credential-already-in-use') {
-      // Show dialog to choose whether to cancel or just sign in
-      final discardExisting = await showDialog<bool>(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Error!'),
-            content: SingleChildScrollView(
-              child: ListBody(
-                children: [
-                  Text('That account is already linked!'
-                      ' Do you want to sign into it instead?'
-                      ' (will lose existing data)'),
-                ],
-              ),
-            ),
-            actions: <Widget>[
-              FlatButton(
-                child: Text('Sign In'),
-                onPressed: () {
-                  Navigator.of(context).pop(true);
-                },
-              ),
-              FlatButton(
-                child: Text('Cancel'),
-                onPressed: () {
-                  Navigator.of(context).pop(false);
-                },
-              ),
-            ],
-          );
-        },
-      );
-
-      if (discardExisting) {
-        // Delete anonymous account
-        await FirebaseAuth.instance.currentUser.delete();
-
-        // Sign in with previously authenticated google account
-        await FirebaseAuth.instance.signInWithCredential(googleCredential);
-      }
-
-      _isChangingAuth = false;
+      _promptSignIn(context, googleCredential);
       return;
     }
   }
@@ -77,11 +35,20 @@ Future<void> linkGoogleAuth(BuildContext context) async {
   print("Linked google account");
 }
 
+class EmailAuthDetails {
+  final String email;
+  final String pass;
+  const EmailAuthDetails(this.email, this.pass);
+}
+
 Future<void> linkEmailAuth(BuildContext context) async {
-  await showDialog(
+  final authDetails = await showDialog<EmailAuthDetails>(
     context: context,
     builder: (context) {
       final node = FocusScope.of(context);
+      final email = TextEditingController();
+      final pass = TextEditingController();
+
       return AlertDialog(
         title: Text('Email Sign In'),
         content: SingleChildScrollView(
@@ -92,9 +59,11 @@ Future<void> linkEmailAuth(BuildContext context) async {
                   prefixIcon: Icon(Icons.email),
                   labelText: 'Email',
                   hintText: 'you@example.com',
+                  errorText: 'test error',
                 ),
                 keyboardType: TextInputType.emailAddress,
                 onEditingComplete: () => node.nextFocus(),
+                controller: email,
               ),
               TextField(
                 decoration: InputDecoration(
@@ -104,8 +73,10 @@ Future<void> linkEmailAuth(BuildContext context) async {
                 obscureText: true,
                 onSubmitted: (_) {
                   node.unfocus();
-                  Navigator.of(context).pop();
+                  Navigator.of(context)
+                      .pop(EmailAuthDetails(email.text, pass.text));
                 },
+                controller: pass,
               ),
             ],
           ),
@@ -114,17 +85,85 @@ Future<void> linkEmailAuth(BuildContext context) async {
           FlatButton(
             child: Text('Sign In'),
             onPressed: () {
-              Navigator.of(context).pop();
+              Navigator.of(context)
+                  .pop(EmailAuthDetails(email.text, pass.text));
             },
           ),
           FlatButton(
             child: Text('Cancel'),
             onPressed: () {
-              Navigator.of(context).pop();
+              Navigator.of(context).pop(null);
             },
           ),
         ],
       );
     },
   );
+
+  // Dialog was canceled
+  if (authDetails == null) return;
+
+  AuthCredential credential;
+  try {
+    // Create account for auth details
+    credential = EmailAuthProvider.credential(
+        email: authDetails.email, password: authDetails.pass);
+
+    // Link email account to existing anonymous account
+    await FirebaseAuth.instance.currentUser.linkWithCredential(credential);
+  } on FirebaseAuthException catch (e) {
+    print('Account link failed with error code: ${e.code}');
+    // Account is already linked to another user
+    if (e.code == 'credential-already-in-use') {
+      _promptSignIn(context, credential);
+      return;
+    }
+  }
+}
+
+Future<void> _promptSignIn(
+    BuildContext context, AuthCredential signInCredential) async {
+  // Show dialog to choose whether to cancel or just sign in
+  final discardExisting = await showDialog<bool>(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Error!'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: [
+              Text('That account is already linked!'
+                  ' Do you want to sign into it instead?'
+                  ' (will lose existing data)'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Sign In'),
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+          ),
+          FlatButton(
+            child: Text('Cancel'),
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+          ),
+        ],
+      );
+    },
+  );
+
+  if (discardExisting) {
+    // Delete anonymous account
+    await FirebaseAuth.instance.currentUser.delete();
+
+    // Sign in with authenticated account
+    await FirebaseAuth.instance.signInWithCredential(signInCredential);
+  }
+
+  _isChangingAuth = false;
 }
