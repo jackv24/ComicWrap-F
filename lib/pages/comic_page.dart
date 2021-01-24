f2bfbfd3d2fe64372fa7f0670c57a60672fb376c
Jack Vine <jackv949@gmail.com>
2021-01-24 13:08:31 +1030

    Implemented pagination for loading comic pages

--- lib/pages/comic_page.dart
+++ lib/pages/comic_page.dart
@@ -1,43 +1,119 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ComicPage extends StatelessWidget {
+class ComicPage extends StatefulWidget {
   final DocumentSnapshot doc;
 
   const ComicPage(this.doc, {Key key}) : super(key: key);
 
   @override
+  _ComicPageState createState() => _ComicPageState();
+}
+
+class _ComicPageState extends State<ComicPage> {
+  List<DocumentSnapshot> pages = [];
+  bool isLoading = false;
+  bool hasMore = true;
+  int documentLimit = 15;
+  DocumentSnapshot lastDocument;
+  ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+
+    // Scrollview won't build if we don't have any pages
+    _getPages();
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final doc = widget.doc;
     final data = doc.data();
-    final collectionRef =
-        doc.reference.collection('pages').orderBy('index', descending: true);
+
+    _scrollController.addListener(() {
+      double maxScroll = _scrollController.position.maxScrollExtent;
+      double currentScroll = _scrollController.position.pixels;
+
+      // Fetch more documents if user scrolls 20% of device height
+      double delta = MediaQuery.of(context).size.height * 0.2;
+      if (maxScroll - currentScroll <= delta) {
+        _getPages();
+      }
+    });
 
     return Scaffold(
       appBar: AppBar(
         title: Text(data['name'] ?? doc.id),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: collectionRef.snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) return Text('Error');
-
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text("Loading...");
-          }
-
-          final docs = snapshot.data.docs;
-          return ListView.builder(
-            itemCount: docs.length,
-            itemBuilder: (context, index) {
-              final doc = docs[index];
-              final data = doc.data();
-              return ListTile(
-                title: Text(data['text'] ?? '!!Page $index!!'),
-              );
-            },
-          );
-        },
+      body: Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        children: [
+          pages.length == 0
+              ? Center(child: Text('No pages...'))
+              : ListView.builder(
+                  controller: _scrollController,
+                  itemCount: pages.length,
+                  itemBuilder: (context, index) {
+                    final data = pages[index].data();
+                    return ListTile(
+                      title: Text(data['text'] ?? '!!Page $index!!'),
+                    );
+                  },
+                ),
+          isLoading
+              ? Container(
+                  padding: EdgeInsets.all(12),
+                  alignment: AlignmentDirectional.bottomCenter,
+                  child: CircularProgressIndicator(),
+                )
+              : Container(),
+        ],
       ),
     );
+  }
+
+  void _getPages() async {
+    if (!hasMore) {
+      return;
+    }
+
+    if (isLoading) {
+      return;
+    }
+
+    print('Loading more pages');
+
+    setState(() {
+      isLoading = true;
+    });
+
+    final query = widget.doc.reference
+        .collection('pages')
+        .orderBy('index', descending: true);
+
+    QuerySnapshot querySnapshot;
+    if (lastDocument == null) {
+      querySnapshot = await query.limit(documentLimit).get();
+    } else {
+      querySnapshot = await query
+          .startAfterDocument(lastDocument)
+          .limit(documentLimit)
+          .get();
+    }
+
+    if (querySnapshot.docs.length < documentLimit) {
+      hasMore = false;
+    }
+
+    final docs = querySnapshot.docs;
+    lastDocument = docs.last;
+    pages.addAll(docs);
+
+    setState(() {
+      isLoading = false;
+    });
   }
 }
