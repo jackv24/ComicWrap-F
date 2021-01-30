43ca5f2213a9d04b36034011b537c7956046be94
Jack Vine <jackv949@gmail.com>
2021-01-30 20:02:25 +1030

    Navigating within websites updates appbar title

--- lib/pages/comic_web_page.dart
+++ lib/pages/comic_web_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -15,32 +16,65 @@ class ComicWebPage extends StatefulWidget {
 }
 
 class _ComicWebPageState extends State<ComicWebPage> {
+  StreamSubscription<DocumentSnapshot> getNewPageSub;
+  DocumentSnapshot newPage;
+
+  String _initialUrl;
+
   @override
   void initState() {
     // Enable hybrid composition on Android
     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+
+    // Construct url from comic and page ID
+    final rootUrl = 'https://${widget.comicDoc.id}/';
+    final pagePath = widget.pageDoc.id.trim().replaceAll(' ', '/');
+    _initialUrl = rootUrl + pagePath;
 
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final pageData = widget.pageDoc.data();
-
-    // Construct url from comic and page IDs
-    final rootUrl = 'https://${widget.comicDoc.id}/';
-    final pagePath = widget.pageDoc.id.trim().replaceAll(' ', '/');
-    final wholeUrl = rootUrl + pagePath;
+    final pageData = newPage?.data();
+    final pageTitle = pageData != null ? pageData['text'] : '';
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(pageData['text'] ?? 'NULL'),
+        title: Text(pageTitle),
       ),
       body: WebView(
-        initialUrl: wholeUrl,
+        initialUrl: _initialUrl,
         javascriptMode: JavascriptMode.unrestricted,
-        // TODO: switch pageDoc over to new page
-        onPageStarted: (currentPage) => print(currentPage),
+        onPageStarted: (currentPage) {
+          // We no longer need the data from the previous new page
+          if (getNewPageSub != null) {
+            getNewPageSub.cancel();
+            getNewPageSub = null;
+          }
+
+          final pageId = currentPage.split('/').skip(3).join(' ');
+          if (newPage != null && pageId == newPage.id) {
+            // Don't trigger rebuild if we haven't changed page
+            print('Already on page: ' + pageId);
+            return;
+          } else {
+            print('Navigating to page: ' + pageId);
+          }
+
+          // Get data for the new page
+          getNewPageSub = widget.comicDoc.reference
+              .collection('pages')
+              .doc(pageId)
+              .get()
+              .asStream()
+              .listen((event) {
+            setState(() {
+              newPage = event;
+              print('Got data for page: ' + pageId);
+            });
+          });
+        },
       ),
     );
   }
