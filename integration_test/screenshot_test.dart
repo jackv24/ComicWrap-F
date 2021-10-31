96cb286130716c9b01b59719c1b1da062c1fa293
Jack Vine <jackv24@users.noreply.github.com>
2021-10-31 20:24:24 +1030

    Started adding promo screenshots to screenshot_test

--- integration_test/screenshot_test.dart
+++ integration_test/screenshot_test.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:comicwrap_f/main.dart';
+import 'package:comicwrap_f/models/firestore/shared_comic.dart';
+import 'package:comicwrap_f/models/firestore/user_comic.dart';
 import 'package:comicwrap_f/utils/auth.dart';
 import 'package:comicwrap_f/utils/database.dart';
 import 'package:comicwrap_f/utils/firebase.dart';
@@ -15,83 +18,92 @@ import 'package:mockito/mockito.dart';
 
 import 'screenshot_test.mocks.dart';
 
-@GenerateMocks([User])
+@GenerateMocks([User, DocumentSnapshot])
 Future<void> main() async {
   final binding = IntegrationTestWidgetsFlutterBinding();
 
-  testWidgets('loginScreen', (tester) async {
-    await tester.pumpWidget(_getCleanState(
-      child: const MyApp(),
-      extraOverrides: [
-        userChangesProvider.overrideWithValue(const AsyncValue.data(null)),
-      ],
-    ));
-
-    await _takeScreenshot(binding, tester);
+  group('promo', () {
+    testWidgets('0_libraryScreen', (tester) async {
+      await _pumpPromoMock(tester);
+      await _takeScreenshot(binding, tester, 'promo');
+    });
   });
 
-  testWidgets('emailSignUpScreen', (tester) async {
-    await tester.pumpWidget(_getCleanState(
-      child: const MyApp(),
-      extraOverrides: [
-        userChangesProvider.overrideWithValue(const AsyncValue.data(null)),
-      ],
-    ));
+  group('test', () {
+    testWidgets('loginScreen', (tester) async {
+      await tester.pumpWidget(_getCleanState(
+        child: const MyApp(),
+        extraOverrides: [
+          userChangesProvider.overrideWithValue(const AsyncValue.data(null)),
+        ],
+      ));
 
-    await tester.pumpAndSettle();
-    final finder = find.widgetWithText(TextButton, 'Sign Up with Email');
-    await tester.tap(finder);
+      await _takeScreenshot(binding, tester, 'test');
+    });
 
-    await _takeScreenshot(binding, tester);
-  });
+    testWidgets('emailSignUpScreen', (tester) async {
+      await tester.pumpWidget(_getCleanState(
+        child: const MyApp(),
+        extraOverrides: [
+          userChangesProvider.overrideWithValue(const AsyncValue.data(null)),
+        ],
+      ));
 
-  testWidgets('emailVerifyScreen', (tester) async {
-    final user = MockUser();
-    when(user.email).thenReturn('test@test.com');
-    when(user.emailVerified).thenReturn(false);
+      await tester.pumpAndSettle();
+      final finder = find.widgetWithText(TextButton, 'Sign Up with Email');
+      await tester.tap(finder);
 
-    await tester.pumpWidget(_getCleanState(
-      child: const MyApp(),
-      extraOverrides: [
-        userChangesProvider.overrideWithValue(AsyncValue.data(user)),
-      ],
-    ));
+      await _takeScreenshot(binding, tester, 'test');
+    });
 
-    await _takeScreenshot(binding, tester);
-  });
+    testWidgets('emailVerifyScreen', (tester) async {
+      final user = MockUser();
+      when(user.email).thenReturn('test@test.com');
+      when(user.emailVerified).thenReturn(false);
 
-  testWidgets('libraryScreen', (tester) async {
-    final user = MockUser();
-    when(user.emailVerified).thenReturn(true);
+      await tester.pumpWidget(_getCleanState(
+        child: const MyApp(),
+        extraOverrides: [
+          userChangesProvider.overrideWithValue(AsyncValue.data(user)),
+        ],
+      ));
 
-    await tester.pumpWidget(_getCleanState(
-      child: const MyApp(),
-      extraOverrides: [
-        userChangesProvider.overrideWithValue(AsyncValue.data(user)),
-        userComicsListProvider.overrideWithValue(const AsyncValue.data(null)),
-      ],
-    ));
+      await _takeScreenshot(binding, tester, 'test');
+    });
 
-    await _takeScreenshot(binding, tester);
-  });
+    testWidgets('libraryScreen', (tester) async {
+      final user = MockUser();
+      when(user.emailVerified).thenReturn(true);
 
-  testWidgets('settingsScreen', (tester) async {
-    final user = MockUser();
-    when(user.emailVerified).thenReturn(true);
+      await tester.pumpWidget(_getCleanState(
+        child: const MyApp(),
+        extraOverrides: [
+          userChangesProvider.overrideWithValue(AsyncValue.data(user)),
+          userComicsListProvider.overrideWithValue(const AsyncValue.data(null)),
+        ],
+      ));
 
-    await tester.pumpWidget(_getCleanState(
-      child: const MyApp(),
-      extraOverrides: [
-        userChangesProvider.overrideWithValue(AsyncValue.data(user)),
-        userComicsListProvider.overrideWithValue(const AsyncValue.data(null)),
-      ],
-    ));
+      await _takeScreenshot(binding, tester, 'test');
+    });
 
-    await tester.pumpAndSettle();
-    final finder = find.widgetWithIcon(IconButton, Icons.settings_rounded);
-    await tester.tap(finder);
+    testWidgets('settingsScreen', (tester) async {
+      final user = MockUser();
+      when(user.emailVerified).thenReturn(true);
 
-    await _takeScreenshot(binding, tester);
+      await tester.pumpWidget(_getCleanState(
+        child: const MyApp(),
+        extraOverrides: [
+          userChangesProvider.overrideWithValue(AsyncValue.data(user)),
+          userComicsListProvider.overrideWithValue(const AsyncValue.data(null)),
+        ],
+      ));
+
+      await tester.pumpAndSettle();
+      final finder = find.widgetWithIcon(IconButton, Icons.settings_rounded);
+      await tester.tap(finder);
+
+      await _takeScreenshot(binding, tester, 'test');
+    });
   });
 }
 
@@ -106,8 +118,8 @@ Widget _getCleanState({required Widget child, List<Override>? extraOverrides}) {
   );
 }
 
-Future<void> _takeScreenshot(
-    IntegrationTestWidgetsFlutterBinding binding, WidgetTester tester) async {
+Future<void> _takeScreenshot(IntegrationTestWidgetsFlutterBinding binding,
+    WidgetTester tester, String subFolder) async {
   // Special per-platform setup + name
   final String platformName;
   if (!kIsWeb) {
@@ -124,5 +136,35 @@ Future<void> _takeScreenshot(
   }
 
   await tester.pumpAndSettle();
-  await binding.takeScreenshot('$platformName-${tester.testDescription}');
+  await binding
+      .takeScreenshot('$subFolder/$platformName/${tester.testDescription}');
+}
+
+Future<void> _pumpPromoMock(WidgetTester tester) async {
+  final user = MockUser();
+  when(user.emailVerified).thenReturn(true);
+
+  final userDoc1 = MockDocumentSnapshot<UserComicModel>();
+  when(userDoc1.id).thenReturn('www.test1.com');
+  when(userDoc1.data()).thenReturn(UserComicModel());
+  final userDoc2 = MockDocumentSnapshot<UserComicModel>();
+  when(userDoc2.id).thenReturn('www.test2.com');
+  when(userDoc2.data()).thenReturn(UserComicModel());
+  final docs = [userDoc1, userDoc2];
+
+  await tester.pumpWidget(_getCleanState(
+    child: const MyApp(),
+    extraOverrides: [
+      userChangesProvider.overrideWithValue(AsyncValue.data(user)),
+      userComicsListProvider.overrideWithValue(AsyncValue.data(docs)),
+      sharedComicFamily(userDoc1.id).overrideWithValue(AsyncValue.data(
+          SharedComicModel(scrapeUrl: userDoc1.id, name: 'Test 1'))),
+      sharedComicFamily(userDoc2.id)
+          .overrideWithValue(AsyncValue.data(SharedComicModel(
+        scrapeUrl: userDoc2.id,
+        name: 'Test 2',
+        isImporting: true,
+      ))),
+    ],
+  ));
 }
