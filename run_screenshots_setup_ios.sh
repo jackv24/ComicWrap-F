2ae2a7521e86602b6a10680f4f3c2b5f92b37863
Jack Vine <jackv24@users.noreply.github.com>
2022-06-13 12:25:45 +0930

    Added iOS Google ads SKAdNetwork stuff

--- run_screenshots_setup_ios.sh
+++ run_screenshots_setup_ios.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-flutter build ios --config-only integration_test/screenshot_test.dart
+flutter build ios --config-only integration_test/screenshot_test.dart \
+--dart-define DISABLE_ADS=true
